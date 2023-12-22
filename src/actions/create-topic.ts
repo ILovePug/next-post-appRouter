63022bf8ede39e4f6a936db7bbe7ecc40d842cd4
Qian Weng <qianweng@qians-mbp.lan>
2023-12-22 12:24:07 -0500

    add form validation with zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,50 @@
 'use server';
 
-export async function createTopic() {
-  // to do: relivate the home page after creating a topic
+import { z } from 'zod';
+import { auth } from '@/auth';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Numst be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[]; // general errors on the form
+  };
+}
+export async function createTopic(
+  _: CreateTopicFormState, // the form state to be passed between form and server action
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  // run validation
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['you must be signed in to do this.'],
+      },
+    };
+  }
+  return {
+    errors: {},
+  };
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import TopicCreateFrom from '@/components/topics/topic-create-form';
+
 export default async function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <div className="text-xl-m2">
+          <h1 className="text-xl m-2">Top Posts</h1>
+        </div>
+      </div>
+      <div>
+        <TopicCreateFrom />
+      </div>
+    </div>
+  );
 }
