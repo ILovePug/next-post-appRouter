053ad8ef854c7b7039242f49f0ad2179b11a631f
Qian Weng <qianweng@qians-mbp.lan>
2023-12-23 08:37:56 -0500

    create post page and pass slug as arg into serve action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import TopicCreateFrom from '@/components/topics/topic-create-form';
-
+import TopicList from '@/components/topics/topic-list';
+import { Divider } from '@nextui-org/react';
 export default async function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
@@ -8,8 +9,11 @@ export default async function Home() {
           <h1 className="text-xl m-2">Top Posts</h1>
         </div>
       </div>
-      <div>
+      <div className="border shadow py-3 px-2">
         <TopicCreateFrom />
+        <Divider className="my-2" />
+        <h3 className="text-lg">Topics</h3>
+        <TopicList />
       </div>
     </div>
   );
